bdae764f85908910588a93137ce00b98689feff2
Thomas Pflüger <tpfluege@ngncc.de>
2025-03-02 17:57:10 +0100

    add rotating track title

--- ui/termui.go
+++ ui/termui.go
@@ -25,115 +25,13 @@ type Display struct {
 	StateChan         <-chan client.State
 	State             client.State
 	uiEventsChan      <-chan ui.Event
+	stringRotate      *stringRotate
 	uiHeader          *widgets.Paragraph
 	uiFooterLeft      *widgets.Paragraph
 	uiFooterRight     *widgets.Gauge
 	uiTrackDetails    *widgets.List
 	uiPlaybackDetails *widgets.List
 	uiPlaybackGuage   *widgets.Gauge
-}
-
-func (d *Display) Draw() {
-	clock_ticker := time.NewTicker(time.Second).C
-	for {
-		select {
-		case e := <-d.uiEventsChan:
-			switch e.ID {
-			case "q", "<C-c>":
-				d.UiDoneChan <- true
-			}
-		case state := <-d.StateChan:
-			if state != d.State {
-				d.State = state
-				d.update()
-			}
-		case <-clock_ticker:
-			d.uiHeader.Text = getHeaderString()
-			ui.Render(d.uiHeader)
-		case <-d.DoneChan:
-			d.Close()
-		}
-	}
-}
-
-type PlayDuration struct {
-	time.Duration
-}
-
-func (d PlayDuration) String() string {
-	hours := float64(int(d.Hours()))
-	minutes := float64(int(d.Minutes()))
-	seconds := d.Seconds() - float64(int(minutes)*60)
-	if hours >= 1 {
-		minutes = d.Minutes() - hours*60
-		seconds = d.Seconds() - hours*3600 - float64(int(minutes*60))
-		return fmt.Sprintf("%02d:%02d:%02d", int(hours), int(minutes), int(seconds))
-	}
-	return fmt.Sprintf("%02d:%02d", int(minutes), int(seconds))
-}
-
-func (d *Display) updatePlaybackGauge() {
-	current_duration := PlayDuration{Duration: time.Duration(d.State.Seek) * time.Millisecond}
-	total_duration := PlayDuration{Duration: time.Duration(d.State.Duration) * time.Second}
-
-	label_string := fmt.Sprintf("%s - %s", current_duration, total_duration)
-	if d.State.Duration == 0 {
-		label_string = fmt.Sprintf("%s", current_duration)
-	}
-	d.uiPlaybackGuage.Label = label_string
-}
-
-func (d *Display) update() {
-	d.uiFooterLeft.Text = d.getIp()
-	d.uiFooterRight.Percent = d.State.Volume
-	d.uiFooterRight.Label = fmt.Sprintf("%d", d.uiFooterRight.Percent)
-	d.uiTrackDetails.Rows = d.getTrackDetails()
-	d.uiPlaybackDetails.Rows = d.getPlaybackDetails()
-	d.uiPlaybackGuage.Percent = d.getElapsedPercent(d.State.Seek, d.State.Duration)
-	d.updatePlaybackGauge()
-
-	ui.Render(d.uiFooterLeft, d.uiFooterRight, d.uiTrackDetails, d.uiPlaybackDetails, d.uiPlaybackGuage)
-}
-
-func (d *Display) getTrackDetails() []string {
-	return []string{d.State.BitDepth, d.State.SampleRate, d.State.TrackType, d.State.Service}
-}
-
-func (d *Display) getPlaybackDetails() []string {
-	return []string{d.State.Title, d.State.Album, d.State.Artist}
-}
-
-func getHeaderString() string {
-	return fmt.Sprintf("%s%31s", "VOLUMIO", time.Now().Format("2006-01-02 15:04"))
-}
-
-func (d *Display) getIp() string {
-	iface_cmd := "ip link show | grep 'state UP' | grep -Po '\\b[a-z]{3,}\\d[a-z]\\d\\b|\\b[a-z]{3,}\\d\\b'"
-	iface, _ := exec.Command("bash", "-c", iface_cmd).Output()
-	iface_str := strings.TrimSuffix(string(iface), "\n")
-	cmd := fmt.Sprintf("ip addr show %s | grep -Po 'inet \\K[\\d.]+'", iface_str)
-	out, _ := exec.Command("bash", "-c", cmd).Output()
-
-	return strings.TrimSuffix(string(out), "\n") + fmt.Sprintf(" (%s)", iface_str)
-}
-
-func (d *Display) getWifiSignal() int {
-	cmd := "iw dev wlp5s0 link | grep -Po 'signal: -\\K[\\d]+'"
-	out, _ := exec.Command("bash", "-c", cmd).Output()
-
-	signal, _ := strconv.Atoi(strings.TrimSuffix(string(out), "\n"))
-
-	return signal
-}
-
-func (d *Display) getElapsedPercent(seek int, duration int) int {
-	seek_sec := int(seek / 1000)
-
-	if duration == 0 {
-		return 100
-	}
-
-	return int((float64(seek_sec) / float64(duration)) * 100)
 }
 
 // TODO: singleton?
@@ -157,6 +55,19 @@ func NewUi(wg *sync.WaitGroup, doneChan <-chan bool, stateChan chan client.State
 		uiEventsChan: ui.PollEvents(),
 		UiDoneChan:   uiDoneChan,
 	}
+
+	// object to rotate title string
+	title_string := stringRotate{
+		stringPadding: "    ",
+		ticker:        *time.NewTicker(500 * time.Millisecond),
+		increment:     1,
+		doneChan:      display.DoneChan,
+		stringChan:    make(chan string),
+	}
+	display.stringRotate = &title_string
+
+	go display.stringRotate.rotateString()
+
 	// header
 	display.uiHeader = widgets.NewParagraph()
 	header_string := fmt.Sprintf("%s%31s", "VOLUMIO", time.Now().Format("2006-01-02 15:04"))
@@ -222,8 +133,158 @@ func NewUi(wg *sync.WaitGroup, doneChan <-chan bool, stateChan chan client.State
 
 func (d *Display) Close() {
 	d.InfoLog.Println("closing ui...")
+	d.stringRotate.close()
 	ui.Close()
 	return
+}
+
+func (d *Display) Draw() {
+	clock_ticker := time.NewTicker(time.Second).C
+	for {
+		select {
+		case e := <-d.uiEventsChan:
+			switch e.ID {
+			case "q", "<C-c>":
+				d.UiDoneChan <- true
+			}
+		case state := <-d.StateChan:
+			if state != d.State {
+				d.State = state
+				// check title string
+				d.stringRotate.update(d.State.Title)
+				// update display
+				d.update()
+			}
+		case title := <-d.stringRotate.stringChan:
+			d.State.Title = title
+			d.uiPlaybackDetails.Rows = d.getPlaybackDetails()
+			ui.Render(d.uiPlaybackDetails)
+		case <-clock_ticker:
+			d.uiHeader.Text = getHeaderString()
+			ui.Render(d.uiHeader)
+		case <-d.DoneChan:
+			d.Close()
+		}
+	}
+}
+
+type PlayDuration struct {
+	time.Duration
+}
+
+func (d PlayDuration) String() string {
+	hours := float64(int(d.Hours()))
+	minutes := float64(int(d.Minutes()))
+	seconds := d.Seconds() - float64(int(minutes)*60)
+	if hours >= 1 {
+		minutes = d.Minutes() - hours*60
+		seconds = d.Seconds() - hours*3600 - float64(int(minutes*60))
+		return fmt.Sprintf("%02d:%02d:%02d", int(hours), int(minutes), int(seconds))
+	}
+	return fmt.Sprintf("%02d:%02d", int(minutes), int(seconds))
+}
+
+func (d *Display) updatePlaybackGauge() {
+	current_duration := PlayDuration{Duration: time.Duration(d.State.Seek) * time.Millisecond}
+	total_duration := PlayDuration{Duration: time.Duration(d.State.Duration) * time.Second}
+
+	label_string := fmt.Sprintf("%s - %s", current_duration, total_duration)
+	if d.State.Duration == 0 {
+		label_string = fmt.Sprintf("%s", current_duration)
+	}
+	d.uiPlaybackGuage.Label = label_string
+}
+
+func (d *Display) update() {
+	d.uiFooterLeft.Text = d.getIp()
+	d.uiFooterRight.Percent = d.State.Volume
+	d.uiFooterRight.Label = fmt.Sprintf("%d", d.uiFooterRight.Percent)
+	d.uiTrackDetails.Rows = d.getTrackDetails()
+	d.uiPlaybackGuage.Percent = d.getElapsedPercent(d.State.Seek, d.State.Duration)
+	d.updatePlaybackGauge()
+
+	ui.Render(d.uiFooterLeft, d.uiFooterRight, d.uiTrackDetails, d.uiPlaybackDetails, d.uiPlaybackGuage)
+}
+
+func (d *Display) getTrackDetails() []string {
+	return []string{d.State.BitDepth, d.State.SampleRate, d.State.TrackType, d.State.Service}
+}
+
+func (d *Display) getPlaybackDetails() []string {
+	return []string{d.State.Title, d.State.Album, d.State.Artist}
+}
+
+// rotate a string based on a ticker event
+type stringRotate struct {
+	originalString string      // copy of original string
+	currentString  string      // copy of rotated string
+	currentPostion int         // rotation position
+	stringLength   int         // length of original string
+	stringPadding  string      // padding of the rotated string
+	ticker         time.Ticker // rotation ticker
+	increment      int         // rotation increment
+	doneChan       <-chan bool // reciever of done event
+	stringChan     chan string // sender of rotated string
+}
+
+func (s *stringRotate) rotateString() {
+	ticker_chan := s.ticker.C
+	for {
+		select {
+		case <-ticker_chan:
+			if len(s.currentString) > 0 {
+				s.currentString = s.currentString[s.increment:] + s.currentString[:s.increment]
+				s.stringChan <- s.currentString
+			}
+		case <-s.doneChan:
+			return
+		}
+	}
+}
+
+func (s *stringRotate) update(title string) {
+	if s.originalString != title {
+		s.originalString = title
+		s.currentString = title + s.stringPadding
+	}
+}
+
+func (s *stringRotate) close() {
+	s.ticker.Stop()
+	close(s.stringChan)
+}
+
+func getHeaderString() string {
+	return fmt.Sprintf("%s%31s", "VOLUMIO", time.Now().Format("2006-01-02 15:04"))
+}
+
+func (d *Display) getIp() string {
+	iface_cmd := "ip link show | grep 'state UP' | grep -Po '\\b[a-z]{3,}\\d[a-z]\\d\\b|\\b[a-z]{3,}\\d\\b'"
+	iface, _ := exec.Command("bash", "-c", iface_cmd).Output()
+	iface_str := strings.TrimSuffix(string(iface), "\n")
+	cmd := fmt.Sprintf("ip addr show %s | grep -Po 'inet \\K[\\d.]+'", iface_str)
+	out, _ := exec.Command("bash", "-c", cmd).Output()
+
+	return strings.TrimSuffix(string(out), "\n") + fmt.Sprintf(" (%s)", iface_str)
+}
+
+func (d *Display) getWifiSignal() int {
+	cmd := "iw dev wlp5s0 link | grep -Po 'signal: -\\K[\\d]+'"
+	out, _ := exec.Command("bash", "-c", cmd).Output()
+
+	signal, _ := strconv.Atoi(strings.TrimSuffix(string(out), "\n"))
+
+	return signal
+}
+
+func (d *Display) getElapsedPercent(seek int, duration int) int {
+	seek_sec := int(seek / 1000)
+
+	if duration == 0 {
+		return 100
+	}
+
+	return int((float64(seek_sec) / float64(duration)) * 100)
 }
 
 // updateParagraph := func(count int) {
